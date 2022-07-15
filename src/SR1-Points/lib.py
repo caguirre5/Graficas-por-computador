f9c64d4ac0a79a7de65467ddcc83bc5718109823
caguirre5 <107177715+caguirre5@users.noreply.github.com>
2022-07-14 19:19:40 -0600

    Clase14/7

--- src/SR1-Points/lib.py
+++ src/SR1-Points/lib.py
@@ -1,35 +1,41 @@
-import struct 
+import struct
+from collections import namedtuple
+
+V2 = namedtuple('Point2', ['x', 'y'])
+
 
 def char(c):
-    #1 byte
+    # 1 byte
     return struct.pack('=c', c.encode('ascii'))
 
+
 def word(w):
-    #2 bytes
+    # 2 bytes
     return struct.pack('=h', w)
 
+
 def dword(d):
-    #4 bytes
+    # 4 bytes
     return struct.pack('=i', d)
+
 
 def color(b, g, r):
     return bytes(
-        [int(b * 255), 
-        int(g * 255), 
-        int(r * 255)]
-        )
-
+        [int(b * 255),
+         int(g * 255),
+         int(r * 255)]
+    )
 
 
 class Renderer(object):
-    def __init__(self, width, height): 
+    def __init__(self, width, height):
         self.width = width
         self.height = height
 
         self.clearColor = color(0, 0, 0)
         self.currColor = color(1, 1, 1)
 
-        self.glViewport(0,0,self.width, self.height)
+        self.glViewport(0, 0, self.width, self.height)
 
         self.glClear()
 
@@ -39,54 +45,75 @@ class Renderer(object):
         self.vpW = width
         self.vpH = height
 
-    def glClear(self):         #List comprehension: Asigna color a cada posicion 20pts
-        self.pixels = [[self.clearColor for y in range(self.height)] for x in range(self.width)]
+    def glClear(self):  # List comprehension: Asigna color a cada posicion 20pts
+        self.pixels = [[self.clearColor for y in range(
+            self.height)] for x in range(self.width)]
 
-    def glClearColor(self, r, g, b): 
-        self.clearColor = color(r,g,b)
+    def glClearColor(self, r, g, b):
+        self.clearColor = color(r, g, b)
 
-    def glClearViewport(self, clr = None):
+    def glClearViewport(self, clr=None):
         for x in range(self.vpX, self.vpX + self.vpW):
             for y in range(self.vpY, self.vpY + self.vpH):
-                self.glPoint(x,y,clr)
+                self.glPoint(x, y, clr)
 
-    def glColor(self, r, g, b): 
-        self.currColor = color(r,g,b)
+    def glColor(self, r, g, b):
+        self.currColor = color(r, g, b)
 
-    def glPoint(self, x, y, clr=None): 
+    def glPoint(self, x, y, clr=None):
         if (0 <= x < self.width) and (0 <= y < self.height):
             self.pixels[x][y] = clr or self.currColor
 
     def glPointvp(self, ndcx, ndcy, clr=None):
-        x = int((ndcx + 1) * (self.vpW /2) + self.vpX)
-        y = int((ndcy + 1) * (self.vpH /2) + self.vpY)
+        x = int((ndcx + 1) * (self.vpW / 2) + self.vpX)
+        y = int((ndcy + 1) * (self.vpH / 2) + self.vpY)
 
         self.glPoint(x, y, clr)
 
-
-    def glFinish(self, filename): 
+    def glFinish(self, filename):
         with open(filename, "wb") as file:
             # Header
             file.write(bytes('B'.encode('ascii')))
             file.write(bytes('M'.encode('ascii')))
-            file.write(dword(14 + 40 + (self.width * self.height * 3))) #tama;o del archivo, cantidad de bytes que va a usar en memoria
+            # tama;o del archivo, cantidad de bytes que va a usar en memoria
+            file.write(dword(14 + 40 + (self.width * self.height * 3)))
             file.write(dword(0))
-            file.write(dword(14 + 40)) #El byte donde empieza la informacion de pixeles
+            # El byte donde empieza la informacion de pixeles
+            file.write(dword(14 + 40))
 
             # InfoHeader
             file.write(dword(40))
-            file.write(dword(self.width)) #Width
-            file.write(dword(self.height)) #Height
-            file.write(word(1)) #Planes, solo ocupa 2 bytes
-            file.write(word(24))#Bits Per Pixel
-            file.write(dword(0)) #Compression
-            file.write(dword(14 + 40 + (self.width * self.height * 3))) #Tama;o de la imagen
+            file.write(dword(self.width))  # Width
+            file.write(dword(self.height))  # Height
+            file.write(word(1))  # Planes, solo ocupa 2 bytes
+            file.write(word(24))  # Bits Per Pixel
+            file.write(dword(0))  # Compression
+            # Tama;o de la imagen
+            file.write(dword(14 + 40 + (self.width * self.height * 3)))
             file.write(dword(0))
             file.write(dword(0))
             file.write(dword(0))
             file.write(dword(0))
-            
-              #Color table
+
+            # Color table
             for y in range(self.height):
                 for x in range(self.width):
                     file.write(self.pixels[x][y])
+
+    def glLine(self, v0, v1, clr=None):
+        # Bresenham line algorithm   y= mx +b
+
+        x0 = int(v0.x)
+        x1 = int(v1.y)
+        y0 = int(v0.x)
+        y1 = int(v1.y)
+
+        if x0 == x1 and y0 == y1:
+            self.glPoint(x0, y0, clr)
+            return
+
+        m = (v1.y - v0.y) / (v1.x-v0.x)
+        for x in range(v0.x, v1.x+1):
+            y = int(x * m)
+
+            self.glPoint(x, y)
--- src/SR1-Points/main.py
+++ src/SR1-Points/main.py
@@ -1,8 +1,8 @@
-from lib import Renderer, color
+from lib import Renderer, color, V2
 import random
 
-width= 512
-height= 512 
+width = 512
+height = 512
 rend = Renderer(width, height)
 
 # rend.glClearColor(1,0,0)
@@ -26,68 +26,68 @@ rend = Renderer(width, height)
 #     y = slope * x
 #     rend.glPoint(x,y)
 
-#StarField
-for x in range(width):
-   for y in range(height):
-       if random.random() > 0.999:
-           size = random.randrange(0, 4)
-           brightness = random.random() / 2 + 0.5
-           starColor = color(brightness, brightness, brightness)
-           
-           if size == 0:
-               rend.glPoint(x,y,starColor)
-           if size == 1:
-               rend.glPoint(x,y, starColor)
-               rend.glPoint(x+1,y, starColor)
-               rend.glPoint(x+1,y+1, starColor)
-               rend.glPoint(x,y+1, starColor)
-           if size == 2:
-               rend.glPoint(x,y, starColor)
-               rend.glPoint(x,y+1, starColor)
-               rend.glPoint(x,y-1, starColor)
-               rend.glPoint(x+1,y, starColor)
-               rend.glPoint(x-1,y, starColor)
-              
-           if size == 3:
-               rend.glPoint(x,y, starColor)
-               rend.glPoint(x+1,y, starColor)
-               rend.glPoint(x,y+1, starColor)
-               rend.glPoint(x+1,y+1, starColor)
-               rend.glPoint(x+2,y, starColor)
-               rend.glPoint(x+2,y+1, starColor)
-               rend.glPoint(x,y+2, starColor)
-               rend.glPoint(x+1,y+2, starColor)
-               rend.glPoint(x+2,y+2, starColor)
-               rend.glPoint(x-1,y+1, starColor)
-               rend.glPoint(x+3,y+1, starColor)
-               rend.glPoint(x+1,y-1, starColor)
-               rend.glPoint(x+1,y+3, starColor)
-               rend.glPoint(x-2,y+1, starColor)
-               rend.glPoint(x+4,y+1, starColor)
-               rend.glPoint(x+1,y-2, starColor)
-               rend.glPoint(x+1,y+4, starColor)
-           if size == 4:
-               rend.glPoint(x,y, starColor)
-               rend.glPoint(x+1,y, starColor)
-               rend.glPoint(x,y+1, starColor)
-               rend.glPoint(x+1,y+1, starColor)
-               rend.glPoint(x+2,y, starColor)
-               rend.glPoint(x+2,y+1, starColor)
-               rend.glPoint(x,y+2, starColor)
-               rend.glPoint(x+1,y+2, starColor)
-               rend.glPoint(x+2,y+2, starColor)
-               rend.glPoint(x-1,y+1, starColor)
-               rend.glPoint(x+3,y+1, starColor)
-               rend.glPoint(x+1,y-1, starColor)
-               rend.glPoint(x+1,y+3, starColor)
-               rend.glPoint(x-2,y+1, starColor)
-               rend.glPoint(x+4,y+1, starColor)
-               rend.glPoint(x+1,y-2, starColor)
-               rend.glPoint(x+1,y+4, starColor)
-               rend.glPoint(x-3,y+1, starColor)
-               rend.glPoint(x+5,y+1, starColor)
-               rend.glPoint(x+1,y-3, starColor)
-               rend.glPoint(x+1,y+5, starColor)
+# StarField
+# for x in range(width):
+#    for y in range(height):
+#        if random.random() > 0.999:
+#            size = random.randrange(0, 4)
+#            brightness = random.random() / 2 + 0.5
+#            starColor = color(brightness, brightness, brightness)
+
+#            if size == 0:
+#                rend.glPoint(x,y,starColor)
+#            if size == 1:
+#                rend.glPoint(x,y, starColor)
+#                rend.glPoint(x+1,y, starColor)
+#                rend.glPoint(x+1,y+1, starColor)
+#                rend.glPoint(x,y+1, starColor)
+#            if size == 2:
+#                rend.glPoint(x,y, starColor)
+#                rend.glPoint(x,y+1, starColor)
+#                rend.glPoint(x,y-1, starColor)
+#                rend.glPoint(x+1,y, starColor)
+#                rend.glPoint(x-1,y, starColor)
+
+#            if size == 3:
+#                rend.glPoint(x,y, starColor)
+#                rend.glPoint(x+1,y, starColor)
+#                rend.glPoint(x,y+1, starColor)
+#                rend.glPoint(x+1,y+1, starColor)
+#                rend.glPoint(x+2,y, starColor)
+#                rend.glPoint(x+2,y+1, starColor)
+#                rend.glPoint(x,y+2, starColor)
+#                rend.glPoint(x+1,y+2, starColor)
+#                rend.glPoint(x+2,y+2, starColor)
+#                rend.glPoint(x-1,y+1, starColor)
+#                rend.glPoint(x+3,y+1, starColor)
+#                rend.glPoint(x+1,y-1, starColor)
+#                rend.glPoint(x+1,y+3, starColor)
+#                rend.glPoint(x-2,y+1, starColor)
+#                rend.glPoint(x+4,y+1, starColor)
+#                rend.glPoint(x+1,y-2, starColor)
+#                rend.glPoint(x+1,y+4, starColor)
+#            if size == 4:
+#                rend.glPoint(x,y, starColor)
+#                rend.glPoint(x+1,y, starColor)
+#                rend.glPoint(x,y+1, starColor)
+#                rend.glPoint(x+1,y+1, starColor)
+#                rend.glPoint(x+2,y, starColor)
+#                rend.glPoint(x+2,y+1, starColor)
+#                rend.glPoint(x,y+2, starColor)
+#                rend.glPoint(x+1,y+2, starColor)
+#                rend.glPoint(x+2,y+2, starColor)
+#                rend.glPoint(x-1,y+1, starColor)
+#                rend.glPoint(x+3,y+1, starColor)
+#                rend.glPoint(x+1,y-1, starColor)
+#                rend.glPoint(x+1,y+3, starColor)
+#                rend.glPoint(x-2,y+1, starColor)
+#                rend.glPoint(x+4,y+1, starColor)
+#                rend.glPoint(x+1,y-2, starColor)
+#                rend.glPoint(x+1,y+4, starColor)
+#                rend.glPoint(x-3,y+1, starColor)
+#                rend.glPoint(x+5,y+1, starColor)
+#                rend.glPoint(x+1,y-3, starColor)
+#                rend.glPoint(x+1,y+5, starColor)
 
 # rend.glViewport(int(width / 4), int(height /4), int(width/2), int(height/2))
 
@@ -96,5 +96,10 @@ for x in range(width):
 # rend.glClearViewport(color(1,1,1))
 
 # rend.glPointvp(0,0)
+
+
+v0 = V2(width/2, height/2)
+v1 = V2(width, height/2)
+v1 = V2(width, height)
 
 rend.glFinish("starfield.bmp")
