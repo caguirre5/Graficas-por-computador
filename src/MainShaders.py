ce2fb882285a29a03418795c6ef51f024aefb163
caguirre5 <107177715+caguirre5@users.noreply.github.com>
2022-08-15 11:56:02 -0600

    Done

--- src/MainShaders.py
+++ src/MainShaders.py
@@ -2,17 +2,19 @@ from lib import Renderer, color, V3, V2
 from texture import Texture
 from shaders import flat, gourad
 
-width = 960
+width = 540
 height = 540
 
 rend = Renderer(width, height)
 
+rend.glClearColor(1, 1, 1)
+rend.glClear()
 rend.active_shader = flat
-rend.active_texture = Texture("src/models/model.bmp")
+rend.active_texture = Texture("src/models/ToonTeddy.bmp")
 
-rend.glLoadModel("src/models/model.obj",
-                 translate=V3(width/2, height/2, 0),
-                 rotate=V3(0, 180, 0),
+rend.glLoadModel("src/models/bear.obj",
+                 translate=V3(235, height/5, 0),
+                 rotate=V3(0, 270, 0),
                  scale=V3(200, 200, 200))
 
 rend.glFinish("output.bmp")
--- src/tempCodeRunnerFile.py
+++ src/tempCodeRunnerFile.py
@@ -1,6 +1,1 @@
-m = -0.5
-b = height / 2
-
-for x in range(width):
-    y = int(m * x + b)
-    rend.glPoint(x, y)
+200
